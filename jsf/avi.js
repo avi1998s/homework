b715d7fa5e085b1fd02850133b6ba297317d0f0d
avi1998s <avi6190i@gmail.com>
2023-01-29 14:00:27 +0200

    homwork of array sparate str and num

--- jsf/avi.js
+++ jsf/avi.js
@@ -1,4 +1,9 @@
-let a = 788
-alert (a-200)
-//git config --global user.name "avi1998s"
-//git config --global user.email "avi6190i@gmail.com"
+const ages = ["zvi32", "avi33", "eli16", "bibi40"];
+function splitNUM (test){
+    let result = [];
+    for (i in test){
+        result[Number(test[i].match(/\d+/g))] = test[i].match(/[a-zA-Z]+/g) ;
+    }
+return result;
+}
+console.log(splitNUM(ages));
