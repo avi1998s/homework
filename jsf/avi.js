0f28d0f80a8780276f11b8953aa509bb8998eb35
avi1998s <avi6190i@gmail.com>
2023-01-30 11:59:59 +0200

    30/1/23

--- jsf/avi.js
+++ jsf/avi.js
@@ -1,8 +1,8 @@
-const ages = ["zvi32", "avi33", "eli16", "bibi40"];
+const ages = ["Zvi32", "Avi33", "Eli16", "Bibi40",'[]}{&^0',"**&1",'%%$%2','-*/-+3'];
 function splitNUM (test){
     let result = [];
     for (i in test){
-        result[Number(test[i].match(/\d+/g))] = test[i].match(/[a-zA-Z]+/g) ;
+        result[Number(test[i].match(/\d+/g))]= test[i].match(/[a-zA-Z]+|[\/\-+\(\)\*&^%\$\#\!]+/g) ;
     }
 return result;
 }
