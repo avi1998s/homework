ee940199ef6840cd7485be5d4420a4844335c29e
avi1998s <avi6190i@gmail.com>
2023-02-23 13:14:22 +0200

    homework of 23/2/23

--- jsf/avi.js
+++ jsf/avi.js
@@ -8,3 +8,23 @@ return result;
 }
 console.log(splitNUM(ages));
 // all 
+let names= ["nadav", "maya", "yonatan", "avi", "yossi", "bat el"],
+names1 = names.toString().toUpperCase().split(",");
+for(i of names1){
+    if (i.length>3 && i.length<7){
+        console.log("names of betwen 3-7 length is: " + i);
+    }
+
+    if(i.toString().includes(" ")){
+        let a = i.split(" ")
+        console.log("the position of two names is: " + i.indexOf());
+        console.log("first name: " + a[0])
+        console.log("last name num 2: " + i.slice(i.indexOf(" "),i.length));
+        console.log("last name num 2: " + a[a.length-1])
+    }
+}
+console.log("");
+
+for(i =1;i< names1.length+1;i++){
+    console.log(names1[i-1].toString().concat(" *".repeat(i)));
+}
