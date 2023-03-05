19e6b4f513c57c47404244c397064c1d45a3cdce
avi1998s <avi6190i@gmail.com>
2023-03-05 21:21:24 +0200

    .

--- jsf/avi.js
+++ jsf/avi.js
@@ -1,24 +1,6 @@
-let names = ["moshe", "nir", "aaavi", "nir", "liron", "yaakov", "naaaair"],
-  pro = prompt("enrer letter"),
-  cunter = 0,
-  cunter2 = 0;
-for (i in names) {
-   cunter = 0;
-  if (names[i].includes(pro)) {
-    console.log(names[i]);
-   //  סופר כמה פעמים מופיעה האות בכל שם במערך  
-    for (j in names[i]) {
-      if (pro == names[i][j]) {
-        cunter++;
-      }
-    }
-    console.log(pro + " exsits in " + cunter + " times" + " in " + names[i]);
-   //   סופר כמה פעמים מופיעה האות מהקלט בכל המערך
-    for (y of names[i]) {
-      if (pro == y) {
-        cunter2++;
-      }
-    }
-  }
+let array = ["&","*","%","#","@","$","!","?",]
+let toto =array[Math.random()*array.length]
+console.log(toto);
+function getIcons (input){
+
 }
-console.log("sum of " + pro + " : " + cunter2);
--- jsf/avi2.js
+++ jsf/avi2.js
@@ -1,9 +1,31 @@
-let names = ["moshe", "nir", ,"avi", "nir","liron", "yaakov", "nir"],
-pro = prompt ("enter name"),
-indexNum = names.indexOf(pro),
-cunter=0;
-while( indexNum>-1){
-    cunter++;
-    indexNum = names.indexOf(pro, indexNum+1)
+// let array = ["&","*","%","#","@","$","!","?",];
+// let toto =Math.random()*array.length;
+// let toto2 =Math.round(toto)
+// function getIcons (){
+//     for (i=1;i<7;i++){
+//         console.log(array[toto2].repeat(i));
+//     }
+// }
+// getIcons()
+
+// פונקציה לבדיקת כוכביות במחרוזת
+function test1 (str){
+    let cunter1=0
+    for (i of str){
+        if (i == "*"){
+            cunter1++
+        }
+    }
+    return cunter1;
 }
- console. log(pro + " exsit "+ " : " +cunter + " times");
+let a = test1(prompt("enter *"));
+let b = test1(prompt("enter *"));
+let c = test1("**&^%$^&hghgh***");
+if (a>b && a>c){
+    console.log("the first ster is biger")
+}
+else if (c>b){
+    console.log("the third ster is biger")
+}
+else{console.log("the second ster is biger");}
+
