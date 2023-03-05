2bd7419107531b0ac120445f497c26645acf9138
avi1998s <avi6190i@gmail.com>
2023-03-05 09:50:56 +0200

    .

--- jsf/avi.js
+++ jsf/avi.js
@@ -1,28 +1,105 @@
-// תרגיל 1
-console.log("תרגיל מספר 1");
-console.log(" ");
-let names= ["nadav", "maya", "yonatan", "avi", "yossi", "bat el"],
-names1 = names.toString().toUpperCase().split(",");
-for(i of names1){
-    if (i.length>3 && i.length<7){
-        console.log("names of betwen 3-7 length is: " + i);
-    }
+let cars = "Saab15 45Volvo 47BMW subaru65 Suzuki11 isUZu12 kiA55 miTsuBishi10",
+carsArray = cars.split(" "),
+carsNumbers = [],
+carsStr = [],
+// משתנים בשיביל אופציות 1 ו 2
+Cunter =0,
+longName ="",
+longName2 ="",
+// משתנים לאופציות 4 ו 5 
+updatePRO,
+newCar,
+// משתנה לוייל
+anser;
 
-    if(i.toString().includes(" ")){
-        let a = i.split(" ")
-        console.log("the position of two names is: " + i.indexOf());
-        console.log("first name: " + a[0])
-        console.log("last name num 2: " + i.slice(i.indexOf(" "),i.length));
-        console.log("last name num 2: " + a[a.length-1])
+for (i in carsArray){
+    carsNumbers.push(carsArray[i].match(/\d+/g))
+    carsStr.push(carsArray[i].match(/[a-zA-Z]+/g))
+    carsNumbers=carsNumbers.flat()
+    carsStr=carsStr.flat()
+ 
+}
+while(true){
+    anser =prompt(`שלום לפניך תפריט עם 6 אופציות
+    לקבלת הרכב עם השם הגדול ביותר יש להקיש 1
+    לקבלת המקט הגבוהה ביותר יש להקיש 2 
+    להוספת רכב חדש יש להקיש 3
+    לעדכון מקט של רכב קיים יש להקיש 4
+    לעדכון מקט לרכב ספציפי יש להקיש 5
+    ליציאה מהתפריט יש להקיש 6`)
+    if (anser == 1){
+        for (i in carsStr){
+            if (carsStr[i].length>Cunter){
+                Cunter=carsStr[i].length
+                longName=carsStr[i]
+                longName2=carsNumbers[i]
+            }   
+        }
+        alert(longName2 +" " + longName)
+        Cunter=0
+        longName=""
     }
-}
-console.log("");
+    else if (anser==2){
+        for (i=0;i<carsNumbers.length;i++){
+            if (carsNumbers[i]>Cunter){
+                Cunter=carsNumbers[i]
+                longName=carsStr[carsNumbers.indexOf(Cunter)]
 
-for(i =1;i< names1.length+1;i++){
-    console.log(names1[i-1].concat(" *".repeat(i)));
+            }    
+        }
+        alert(Cunter + " " + longName)
+        Cunter=0
+        longName=""
+    }
+    else if (anser==3){
+        carsArray.unshift(prompt("הכנס רכב עם מספר רכב"))
+        alert(carsArray)
+        carsNumbers.unshift(carsArray[0].match(/\d+/g));
+        carsStr.unshift(carsArray[0].match(/[a-zA-Z]+/g));
+        carsNumbers=carsNumbers.flat();
+        carsStr=carsStr.flat();
+
+    }
+    else if (anser==4){
+        updatePRO=prompt("הכנס שם רכב")
+        if (carsStr.includes(updatePRO)){
+        newCar=prompt("הכנס שם ומספר רכב לעדכון")
+        carsArray[carsStr.indexOf(updatePRO)]=newCar
+        carsNumbers[carsArray.indexOf(newCar)] = newCar.match(/\d+/g);
+        carsStr[carsArray.indexOf(newCar)] = newCar.match(/[a-zA-Z]+/g);
+        alert(carsArray)
+        carsArray= carsArray.flat()
+        carsStr = carsStr.flat()
+        carsNumbers=carsNumbers.flat()
+        }
+        else {alert("הרכב לא נמצא במאגר ")}
+        
+        
+    }
+    else if (anser==5){
+        updatePRO=prompt("הכנס מספר רכב")
+        if (carsNumbers.includes(updatePRO)){
+        newCar= prompt("הכנס שם ומספר רכב לעדכון")
+        carsArray[carsNumbers.indexOf(updatePRO)]=newCar
+        carsStr[carsNumbers.indexOf(updatePRO)] = newCar.match(/[a-zA-Z]+/g);
+        carsNumbers[carsNumbers.indexOf(updatePRO)] = newCar.match(/\d+/g);
+        alert(carsArray)
+        carsArray= carsArray.flat()
+        carsStr = carsStr.flat()
+        carsNumbers=carsNumbers.flat()
+        }
+        else {alert("הרכב לא נמצא במאגר ")}
+        
+    }
+    else if (anser==6){
+        alert("תודה רבה על שימושך בתפריט")
+        break
+    }
+    else if (anser==7){
+        alert(carsNumbers)
+    }
+    else if (anser==8){
+        alert(carsStr)
+    }
+    else{alert("נא להקיש מספרים בין 1-6 בלבד")}
 }
-console.log(" ");
-console.log("תרגיל מספר 2 ");
-// תרגיל מספר 2 
-let txt = "ben##levi###kobi#yossi#avi####dan"
-console.log(txt.toUpperCase().match(/[A-Z]/g).toString().replaceAll(",",""));
